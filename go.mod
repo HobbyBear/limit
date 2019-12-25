843604dc79ed7b16c79015d7d2de1fdc02431424
xiongchuanhong <18881934641@163.com>
2019-12-25 16:54:57 +0800

    hystrix 源码解析

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module limit
 
 go 1.12
 
-require github.com/afex/hystrix-go v0.0.0-20180502004556-fa1af6a1f4f5
+require (
+	github.com/afex/hystrix-go v0.0.0-20180502004556-fa1af6a1f4f5
+	github.com/smartystreets/goconvey v1.6.4 // indirect
+)
--- hystrix/hystrix_test.go
+++ hystrix/hystrix_test.go
@@ -2,8 +2,12 @@ package hystrix
 
 import (
 	"errors"
-	"github.com/afex/hystrix-go/hystrix"
+	"fmt"
+	"sync"
 	"testing"
+	"time"
+
+	"github.com/afex/hystrix-go/hystrix"
 )
 
 func Test01(t *testing.T) {
@@ -19,6 +23,67 @@ func Test01(t *testing.T) {
 
 }
 
+func Test02(t *testing.T) {
+	var one sync.Once
+	one.Do(func() {
+		fmt.Println("hello")
+	})
+	one.Do(func() {
+		fmt.Println("hello2")
+	})
+}
+
 type Counter struct {
-	bucket map[int64]int
+	buckets map[int64]*bucket
+	Rw      sync.RWMutex
+}
+
+type bucket struct {
+	Value int
+}
+
+func (c *Counter) Increment(i int) {
+	if i == 0 {
+		return
+	}
+	c.Rw.Lock()
+	defer c.Rw.Unlock()
+	bucket := c.getCurrentBucket()
+	bucket.Value += i
+	c.removeOldBucket()
+}
+
+// 过去10s内最新的计数值
+func (c *Counter) Sum() int {
+	now := time.Now().Unix() - 10
+	sum := 0
+	c.Rw.RLock()
+	defer c.Rw.Unlock()
+	for timestamp, bucket := range c.buckets {
+		if timestamp >= now {
+			sum += bucket.Value
+		}
+	}
+	return sum
+}
+
+func (c *Counter) getCurrentBucket() *bucket {
+	now := time.Now().Unix()
+	var ok bool
+	var b *bucket
+	if b, ok = c.buckets[now]; !ok {
+		b = new(bucket)
+		c.buckets[now] = b
+	}
+	return b
+
+}
+
+func (c *Counter) removeOldBucket() {
+	now := time.Now().Unix() - 10
+	for timestamp := range c.buckets {
+		if timestamp <= now {
+			delete(c.buckets, timestamp)
+		}
+	}
 }
