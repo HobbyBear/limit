50dfa1181bec0317a0f0902329afd2be8bdabd88
xiongChuanHong <1962494765@qq.com>
2020-01-01 23:40:45 +0800

    fix：释放协程改为channel的方式

--- tasklimit/task_limit.go
+++ tasklimit/task_limit.go
@@ -16,9 +16,8 @@ type Setter func(t *Limit)
 
 type Limit struct {
 	client        *redis.Client
-	rw            sync.RWMutex
 	once          sync.Once
-	exitsWorker   bool
+	exitsWorker   chan int
 	handler       Handler
 	taskQueue     string
 	limitQueue    string
@@ -29,7 +28,7 @@ type Limit struct {
 
 func (t *Limit) Init(setters ...Setter) *Limit {
 	t.once.Do(func() {
-		t.exitsWorker = false
+		t.exitsWorker = make(chan int, 1)
 		for _, setter := range setters {
 			setter(t)
 		}
@@ -79,74 +78,64 @@ func (t *Limit) Do(taskParam interface{}) error {
 }
 
 func (t *Limit) notifyWorker() {
-	t.rw.RLock()
-	if t.exitsWorker {
-		t.rw.RUnlock()
-		return
-	}
-	t.rw.RUnlock()
-	t.rw.Lock()
-	t.exitsWorker = true
-	t.lastTime = time.Now()
-	t.rw.Unlock()
-	log.Println("create new go worker ...")
-	goFunc(func() {
-		ticker := time.NewTicker(time.Second)
-		defer ticker.Stop()
-		for range ticker.C {
-			for {
-				limit, err := t.client.LLen(t.limitQueue).Result()
-				if err != nil {
-					break
-				}
-				if limit > t.rate {
-					ttlTime, err := t.client.TTL(t.limitQueue).Result()
+	select {
+	case t.exitsWorker <- 1:
+		log.Println("create new go worker ...")
+		t.lastTime = time.Now()
+		goFunc(func() {
+			ticker := time.NewTicker(time.Second)
+			defer ticker.Stop()
+			for range ticker.C {
+				for {
+					limit, err := t.client.LLen(t.limitQueue).Result()
 					if err != nil {
 						break
 					}
-					if ttlTime < 0 {
+					if limit > t.rate {
+						ttlTime, err := t.client.TTL(t.limitQueue).Result()
+						if err != nil {
+							break
+						}
+						if ttlTime < 0 {
+							t.client.Expire(t.limitQueue, time.Second)
+						}
+						log.Println("tow many task todo ,please waiting ....")
+						break
+					}
+					count, err := t.client.LPush(t.limitQueue, 0).Result()
+					if err != nil {
+						break
+					}
+					if count == 1 {
 						t.client.Expire(t.limitQueue, time.Second)
 					}
-					log.Println("tow many task todo ,please waiting ....")
-					break
-				}
-				count, err := t.client.LPush(t.limitQueue, 0).Result()
-				if err != nil {
-					break
-				}
-				if count == 1 {
-					t.client.Expire(t.limitQueue, time.Second)
-				}
-				dataStr, err := t.client.RPop(t.taskQueue).Result()
-				if err != nil && err != redis.Nil {
-					break
-				}
-				t.rw.RLock()
-				lastRunningTime := t.lastTime
-				t.rw.RUnlock()
-				if dataStr == "" {
-					if t.cleanDuration < time.Since(lastRunningTime) {
-						log.Println("destroy go worker...")
-						t.rw.Lock()
-						t.exitsWorker = false
-						t.rw.Unlock()
-						return
+					dataStr, err := t.client.RPop(t.taskQueue).Result()
+					if err != nil && err != redis.Nil {
+						break
 					}
-					break
-				}
-				t.rw.Lock()
-				t.lastTime = time.Now()
-				t.rw.Unlock()
-				goFuncWithString(func(dataStr string) {
-					err := t.handler([]byte(dataStr))
-					if err != nil {
-						log.Println("err ... ")
+					lastRunningTime := t.lastTime
+					if dataStr == "" {
+						if t.cleanDuration < time.Since(lastRunningTime) {
+							log.Println("destroy go worker...")
+							<-t.exitsWorker
+							return
+						}
+						break
 					}
-				}, dataStr)
+					t.lastTime = time.Now()
+					goFuncWithString(func(dataStr string) {
+						err := t.handler([]byte(dataStr))
+						if err != nil {
+							log.Println("err ... ")
+						}
+					}, dataStr)
+				}
 			}
-		}
 
-	})
+		})
+	default:
+
+	}
 }
 
 func goFuncWithString(f func(data string), data string) {
