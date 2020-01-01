de35a4018e28ea0a78e4c44b1408f9151a67b79b
xiongChuanHong <1962494765@qq.com>
2020-01-01 11:00:46 +0800

    tasklimit完成

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module limit
 
 go 1.12
 
-require github.com/afex/hystrix-go v0.0.0-20180502004556-fa1af6a1f4f5
+require (
+	github.com/afex/hystrix-go v0.0.0-20180502004556-fa1af6a1f4f5
+	github.com/go-redis/redis v6.15.6+incompatible
+	github.com/onsi/ginkgo v1.11.0 // indirect
+	github.com/onsi/gomega v1.8.1 // indirect
+	github.com/panjf2000/ants v1.3.0
+	github.com/smartystreets/goconvey v1.6.4 // indirect
+)
---
+++ tasklimit/task_limit.go
@@ -0,0 +1,101 @@
+package tasklimit
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/go-redis/redis"
+	"sync"
+	"time"
+)
+
+type Handler func(x interface{}) error
+
+type TaskLimit struct {
+	client      *redis.Client
+	rw          sync.RWMutex
+	exitsWorker bool
+	handler     Handler
+	taskQueue   string
+	limitQueue  string
+	rate        int64
+	lastTime    time.Time
+	cleanTime   time.Duration
+}
+
+func NewLimitTask(client *redis.Client, taskName string, handler Handler, rate int64, clean time.Duration) *TaskLimit {
+	return &TaskLimit{
+		exitsWorker: false,
+		handler:     handler,
+		client:      client,
+		taskQueue:   fmt.Sprintf("%s:task", taskName),
+		limitQueue:  fmt.Sprintf("%s:limit", taskName),
+		rate:        rate,
+		cleanTime:   clean,
+	}
+}
+
+func (t *TaskLimit) Do(task interface{}) {
+	t.rw.RLock()
+	if t.exitsWorker {
+		t.rw.Unlock()
+		goto exitsWorker
+	}
+	t.rw.Lock()
+	t.exitsWorker = true
+	t.lastTime = time.Now()
+	t.rw.Unlock()
+	go func() {
+		var wg WaitGroupWrapper
+		ticker := time.NewTicker(time.Second)
+		defer ticker.Stop()
+		for range ticker.C {
+			if limit, _ := t.client.LLen(t.limitQueue).Result(); limit > t.rate {
+				break
+			}
+			count, _ := t.client.LPush(t.limitQueue, struct{}{}).Result()
+			if count == 1 {
+				t.client.Expire(t.limitQueue, 1*time.Second)
+			}
+			dataStr, _ := t.client.RPop(t.taskQueue).Result()
+			t.rw.RLock()
+			lastRunningTime := t.lastTime
+			t.rw.RUnlock()
+			if dataStr == "" {
+				if t.cleanTime < time.Since(lastRunningTime) {
+					t.rw.Lock()
+					t.exitsWorker = false
+					t.rw.Unlock()
+					wg.Wait()
+					return
+				}
+				break
+			}
+			t.rw.Lock()
+			t.lastTime = time.Now()
+			t.rw.Unlock()
+			wg.Wrap(func(dataStr string) {
+				var x interface{}
+				json.Unmarshal([]byte(dataStr), &x)
+				t.handler(x)
+			}, dataStr)
+		}
+	}()
+exitsWorker:
+	data, _ := json.Marshal(task)
+	count, _ := t.client.LPush(t.taskQueue, data).Result()
+	if count == 1 {
+		t.client.Expire(t.taskQueue, 1*time.Second)
+	}
+}
+
+type WaitGroupWrapper struct {
+	sync.WaitGroup
+}
+
+func (w *WaitGroupWrapper) Wrap(cb func(dataStr string), dataStr string) {
+	w.Add(1)
+	go func() {
+		cb(dataStr)
+		w.Done()
+	}()
+}
